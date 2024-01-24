bb2a9e90e81b25150b1e14c0f04548d80a4436d0
Jan ATAC <can.atac@gmail.com>
2024-01-24 14:28:33 +0000

    Changes on redis

--- main.go
+++ main.go
@@ -9,42 +9,43 @@ import (
 	redis "github.com/go-redis/redis/v8"
 )
 
-var dbUrl = os.Getenv("DB_URL")
-var dbPort = os.Getenv("DB_PORT")
 var serverPort = os.Getenv("SERVER_PORT")
 
 var ctx = context.Background()
 
-func connectToRedis(redisAddr, redisPassword string) *redis.Client {
-    client := redis.NewClient(&redis.Options{
-        Addr:     redisAddr,
-        Password: redisPassword, // no password set
-        DB:       0,  // use default DB
-    })
+func connectToRedis() *redis.Client {
+	redisURI := os.Getenv("REDIS_URI")
+	addr, err := redis.ParseURL(redisURI)
+	if err != nil {
+		panic(err)
+	}
 
-    _, err := client.Ping(ctx).Result()
-    if err != nil {
-        panic(err)
-    }
+	client := redis.NewClient(addr)
 
-    return client
+	_, err = client.Ping(ctx).Result()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return client
 }
 
 func verifyOTP(redisClient *redis.Client, email, otp string) bool {
-    storedOTP, err := redisClient.Get(ctx, email).Result()
-    if err != nil {
-        return false
-    }
+	storedOTP, err := redisClient.Get(ctx, email).Result()
+	if err != nil {
+		return false
+	}
 
-    return otp == storedOTP
+	return otp == storedOTP
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/verify", func(w http.ResponseWriter, r *http.Request) {
 		email := r.URL.Query().Get("email")
 		otp := r.URL.Query().Get("otp")
-	
-		redisClient := connectToRedis(fmt.Sprintf("%s:%s",dbUrl,dbPort), "")
+
+		redisClient := connectToRedis()
 		if verifyOTP(redisClient, email, otp) {
 			redisClient.Del(ctx, email)
 			fmt.Fprint(w, "OTP verified")
@@ -52,6 +53,6 @@ func main(){
 			http.Error(w, "Invalid OTP", http.StatusBadRequest)
 		}
 	})
-	
-	http.ListenAndServe(fmt.Sprintf(":%s",serverPort, nil))
+
+	http.ListenAndServe(fmt.Sprintf(":%s", serverPort, nil))
 }
